760b2f112a88827c1adcaa656bd4a689535e15e0
MajorRedCloud <musiclovermadmp3@gmail.com>
2024-12-23 18:47:46 +0530

    initial project setup, setup routes, setup nativewind

---
+++ app/(root)/(tabs)/explore.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const Explore = () => {
+  return (
+    <View>
+      <Text className='font-bold size-6 text-red-500'>Explore is working right</Text>
+    </View>
+  )
+}
+
+export default Explore
---
+++ app/(root)/(tabs)/index.tsx
@@ -0,0 +1,16 @@
+import { Link } from "expo-router";
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View className="flex-1 justify-center items-center"
+    >
+      <Text className="text-red-800 font-bold my-10">Hello there kenobi</Text>
+      <Link href={'/sign-in'}>Sign-in</Link>
+      <Link href={'/(root)/(tabs)/explore'}>explore</Link>
+      <Link href={'/(root)/(tabs)/profile'}>Profile</Link>
+      <Link href={'/properties/12345'}>Properties</Link>  
+
+    </View>
+  );
+}
